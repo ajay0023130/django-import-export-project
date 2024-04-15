63e73df40772ce82f48ab1f652b3fca585c72a32
ajay0023130 <ajaybabu0046@gmail.com>
2024-04-15 12:44:03 +0530

    first commit

---
+++ importexportadmin/app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin 
+from .resource import ReportResource  
+from .models import Report
+
+class ReportAdmin(ImportExportModelAdmin):
+    resource_class = ReportResource      
+admin.site.register(Report, ReportAdmin)
